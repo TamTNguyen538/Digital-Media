17d545f799a7f2571ed31a8d1f936ae4ab044633
TamTNguyen538 <113304943+TamTNguyen538@users.noreply.github.com>
2023-01-19 13:55:10 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
-  ellipse(50, 50, 80, 80);
+  background(230);
+  ellipse(200,100,50,50);
+  ellipse(400,105,50,50);
+
+  //triangle(300,100,290,200,310,200);
+
+  arc(300,220,200,40,0,PI,PIE,);
+
+  line(175,65,225,65);
+  line(375,70,425,65);
+
+
 }
