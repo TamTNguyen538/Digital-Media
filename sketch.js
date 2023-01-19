c39adc3971dc1052453908ea886f5244a0e6c03d
TamTNguyen538 <113304943+TamTNguyen538@users.noreply.github.com>
2023-01-19 14:04:23 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,28 @@ function setup() {
 }
 
 function draw() {
-  background(230);
+  background(250,0,0);
+
+  
   ellipse(200,100,50,50);
   ellipse(400,105,50,50);
+  
+  push();
+  fill(0);
+  ellipse(200,100,10,10);
+  ellipse(400,105,10,10);
+  pop();
+  
 
+  //drawing the nose
   //triangle(300,100,290,200,310,200);
+  beginShape();
+  vertex(295,100);
+  vertex(280,200);
+  vertex(300,205);
+  vertex(320,200);
+  vertex(305,100);
+  endShape(CLOSE);
 
   arc(300,220,200,40,0,PI,PIE,);
 
