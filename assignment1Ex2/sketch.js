39473fc3ae7bb7bbb6c28516ed4c1678c905b48b
TamTNguyen538 <113304943+TamTNguyen538@users.noreply.github.com>
2023-01-19 22:18:23 -0600

    Update sketch.js

--- assignment1Ex2/sketch.js
+++ assignment1Ex2/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(300);
+  let c = color(255, 0, 0, 100);
+  fill(c);
+  noStroke();
+  ellipse(200, 140, 220, 220);
+
+  c = color(0, 0, 255, 100);
+  fill(c);
+  noStroke();
+  ellipse(130, 260, 220, 220);
+
+  c = color(0, 255, 0, 100);
+  fill(c);
+  noStroke();
+  ellipse(270, 260, 220, 220);
 }
